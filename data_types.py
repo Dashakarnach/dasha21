c4b3301230022cbe49cf7db14add2a825846b673
macbookD <dashakarkar15@gmail.com>
2022-07-30 02:17:21 +0300

    'update gitignore'

---
+++ data_types.py
@@ -0,0 +1,73 @@
+result = 1 + 1
+print(result)
+
+value_str = 15
+value_int = 13
+result = value_str + value_int
+print(result)
+
+variable_str = 4
+variable_int = 10
+result = variable_str - variable_int
+print(result)
+
+value_str = 22
+value_int = 7
+result = value_str * value_int
+print(result)
+
+value_str = 18
+value_int = 3
+result = value_str / value_int
+print(result)
+
+value_int = 9
+value_str = 22
+result = value_int, value_str = 9 , 22
+print(value_int, value_str)
+
+value_str = 3
+value_int = 12
+result = (value_str + value_int)*2
+print(result)
+
+
+value_str = 3
+value_int = 4
+result = (value_str ** value_int)
+print(result)
+
+my_cool_str = "привіт"
+print(my_cool_str)
+my_cool_str = "я даша"
+print(my_cool_str)
+
+value_str = 6
+value_int = 23
+result = (value_str - value_int)**2
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ rows_and_numbers.py
@@ -0,0 +1,30 @@
+username: str = input("Please tell me your name ")
+result = f"Hello , {username}, nice to meet you!"
+print(result)
+
+value_1 = int(4.8)
+print(value_1, type(value_1))
+
+value_1 = float("6.4")
+print(value_1, type(value_1))
+
+value_2 = float("36.0")
+print(value_2, type(value_2))
+
+exponentiation = value_2 ** 4
+print(exponentiation, type(exponentiation))
+
+square_root = value_2 ** 0.5
+print(square_root, type(square_root))
+
+division_remainder = value_2 % value_1
+print(division_remainder, type(division_remainder))
+
+
+
+
+
+
+
+
+
