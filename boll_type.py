813a59811ded4fd5ba2c1a9c1c879c7b59766b19
macbookD <dashakarkar15@gmail.com>
2022-08-03 18:03:33 +0300

    'update gitignore'

---
+++ boll_type.py
@@ -0,0 +1,112 @@
+comparison_result: bool = 3 != 5
+print(comparison_result)
+
+
+
+
+comparison_result: bool = 5 == 5
+print(comparison_result)
+
+comparison_result: bool = 5 >= 5
+print(comparison_result)
+
+comparison_result: bool = 5 <= 5
+print(comparison_result)
+
+
+
+
+bool_none = bool(7)
+print(bool_none)
+
+
+bool_none >= bool(7)
+print(bool_none)
+
+bool_none != bool(7)
+print(bool_none)
+
+bool_none <= bool(7)
+print(bool_none)
+
+bool_none > bool(7)
+print(bool_none)
+
+bool_none < bool(7)
+print(bool_none)
+
+
+
+
+str_venue = bool(10-1)
+print(str_venue)
+
+str_venue > bool(10-1)
+print(str_venue)
+
+
+str_venue <= bool(10-1)
+print(str_venue)
+
+str_venue >= bool(10-1)
+print(str_venue)
+
+str_venue < bool(10-1)
+print(str_venue)
+
+str_venue != bool(10-1)
+print(str_venue)
+
+
+
+logical_negation = (True and True) or False
+print(logical_negation)
+
+logical_negation_1 = (False or True) and True
+print(logical_negation_1)
+
+logical_negation_2 = (True or True) and not False
+print(logical_negation_2)
+
+logical_negation_3 = (False and True) or True
+print(logical_negation_3)
+
+logical_negation_4 = (True and True) and not False
+print(logical_negation_4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ jjjjj.py
@@ -0,0 +1,39 @@
+str_value = "   stEeV "
+print(str_value)
+
+orig_text = "   stEeV "
+print(orig_text.strip())
+
+
+
+str_value = input("Please tell me your name ")
+result = str_value.capitalize()
+print(result)
+
+
+
+new_string = "Steev"
+print("length of string --> ", len(new_string))
+
+
+str_for_slice = "Steev"
+print(str_for_slice[: -6: -1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
